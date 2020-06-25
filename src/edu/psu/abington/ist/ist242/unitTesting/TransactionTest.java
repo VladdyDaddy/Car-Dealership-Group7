89e5de2eaa18100e76edb9882b8d16d293d2e318
nus688 <67125507+nus688@users.noreply.github.com>
2020-06-25 18:58:49 -0400

    Update TransactionTest.java

--- src/edu/psu/abington/ist/ist242/unitTesting/TransactionTest.java
+++ src/edu/psu/abington/ist/ist242/unitTesting/TransactionTest.java
@@ -1,3 +1,12 @@
+/*
+Project: Team Project Design: Create Java Classes
+Purpose Details: The Java application you are building for the car dealership will be all text-based running from the command line.
+Course: IST 242
+Author: Team 7
+Date Developed: 06/10/2020
+Last Date Changed:
+Revision: 4
+*/
 package edu.psu.abington.ist.ist242.unitTesting;
 
 import edu.psu.abington.ist.ist242.Transaction;
